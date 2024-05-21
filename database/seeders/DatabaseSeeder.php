bf7c0993225f5e0d859a89af6382cb9f3564f127
iqbalalnasabil <iqbalalnasabill@gmail.com>
2024-05-21 23:20:57 +0700

    auth laravel

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin123'),
+            'phone_number' => '08123456789',
+            'address' => 'blagung',
+            'role' => 'admin'
+        ]);
+    }
+}
